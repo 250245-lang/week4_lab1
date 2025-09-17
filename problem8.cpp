5adee525c762a8d92879f5fc20d5683ab3817792
KOSMOSTARuzb <94840242+KOSMOSTARuzb@users.noreply.github.com>
2025-09-17 11:59:39 +0500

    change 3

--- problem8.cpp
+++ problem8.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main() {
     char c;
     cin>>c;
-    bool lower = c>=65 && c<=90;
-    bool upper = c>=97 && c<=122;
+    bool upper = c>=65 && c<=90;
+    bool lower = c>=97 && c<=122;
     if(lower) {
         cout<<"Lowercase alphabet"<<endl;
     }else if (upper) {
